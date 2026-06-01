2becca4bc6d77ea70ee0383027ad0a23dfa0d5be
glhfcandyy_ <phamnguyencattuong1999@gmail.com>
2026-06-01 20:47:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2413,7 +2413,7 @@ html,body{margin:0;padding:0;background:transparent;font-family:Arial,Helvetica,
 """
     components.html(
         story_component_css + html,
-        height=7000,
+        height=6800,
         scrolling=False
     )
 
