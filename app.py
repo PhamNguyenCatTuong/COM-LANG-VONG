3c68fc7c3d417f045699551d05e8c2c9fea7febc
glhfcandyy_ <phamnguyencattuong1999@gmail.com>
2026-06-02 21:12:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1471,7 +1471,7 @@ footer {visibility: hidden;}
 
 .recipe-book {
     position: relative;
-    max-width: 920px;
+    max-width: 1000px;
     margin: 8px auto 0;
     min-height: 560px;
     perspective: 1800px;
